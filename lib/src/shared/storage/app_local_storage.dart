3cd79bd8fea6cc067eec47d784a0e76d4e055aaf
DanielMendesDoAmaralLivance <daniel.amaral@livance.com.br>
2025-04-29 15:17:46 -0300

    Melhora lógica de excluird ados da box

--- lib/src/shared/storage/app_local_storage.dart
+++ lib/src/shared/storage/app_local_storage.dart
@@ -1,5 +1,5 @@
 import 'package:fast_location/src/modules/home/model/address_model.dart';
-import 'package:fast_location/src/shared/storage/app_boxes.dart';
+// import 'package:fast_location/src/shared/storage/app_boxes.dart';
 import 'package:hive_flutter/adapters.dart';
 
 class AppLocalStorage {
@@ -7,6 +7,8 @@ class AppLocalStorage {
     await Hive.initFlutter();
 
     _registerAdapters();
+
+    // await _clearBox<AddressModel>(AppBoxes.searchedAddresses);
   }
 
   static Future<Box<T>> openBox<T>(String boxName) async {
@@ -24,7 +26,7 @@ class AppLocalStorage {
   // Used to clear box data, when needed.
   // ignore: unused_element
   static Future<void> _clearBox<T>(String boxName) async {
-    var box = await openBox<AddressModel>(AppBoxes.searchedAddresses);
-    box.deleteAll(box.keys);
+    var box = await openBox<T>(boxName);
+    await box.deleteAll(box.keys);
   }
 }
